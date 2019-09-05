275f90715d62a46b01c32eaeeeaef3f9b96b6d81
JustinSOrtega <justinsortega@gmail.com>
2019-09-05 15:20:52 -0500

    lesson 1 ex done

--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -1,29 +1,32 @@
 package com.codeup.springblog.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class MathController {
     @RequestMapping(path = "/add/{numone}/and/{numtwo}", method = RequestMethod.GET)
     @ResponseBody
-    public String add(@PathVariable int numone, int numtwo) {
-        return "<h1>"+ (numone + numtwo) + "<h1>";
+    public String add(@PathVariable int numone, @PathVariable int numtwo) {
+
+        return "<h1>" + (numone + numtwo)+ "</h1>";
     }
     @RequestMapping(path = "/subtract/{numone}/from/{numtwo}", method = RequestMethod.GET)
     @ResponseBody
-    public String subtract(@PathVariable int numone, int numtwo) {
+    public String subtract(@PathVariable int numone, @PathVariable int numtwo) {
         return "<h1>"+ (numone - numtwo) + "<h1>";
     }
     @RequestMapping(path = "/multiply/{numone}/and/{numtwo}", method = RequestMethod.GET)
     @ResponseBody
-    public String multiply(@PathVariable int numone, int numtwo) {
+    public String multiply(@PathVariable int numone, @PathVariable int numtwo) {
         return "<h1>"+ (numone * numtwo) + "<h1>";
     }
     @RequestMapping(path = "/divide/{numone}/by/{numtwo}", method = RequestMethod.GET)
     @ResponseBody
-    public String divide(@PathVariable int numone, int numtwo) {
+    public String divide(@PathVariable int numone, @PathVariable int numtwo) {
         return "<h1>"+ (numone/numtwo) + "<h1>";
     }
 
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,10 +1,12 @@
 package com.codeup.springblog.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class PostController {
     @GetMapping("/posts")
     @ResponseBody
