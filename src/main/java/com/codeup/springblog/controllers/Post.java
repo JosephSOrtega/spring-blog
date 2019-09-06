f0c46de6c4e676491ce384272655271e2446ba0d
JustinSOrtega <justinsortega@gmail.com>
2019-09-06 14:07:30 -0500

    view ex 2 fixed

---
+++ src/main/java/com/codeup/springblog/controllers/Post.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog.controllers;
+
+import com.sun.xml.internal.xsom.impl.scd.Step;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+class Post {
+
+    String title;
+    String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+}
