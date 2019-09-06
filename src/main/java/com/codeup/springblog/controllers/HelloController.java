c642a60c4f84dcb6772c6359466d086221d870cb
JustinSOrtega <justinsortega@gmail.com>
2019-09-06 11:21:35 -0500

    Mid morning break

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,27 +1,35 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 class HelloController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "<h1>Hello from Spring!</h1>";
+    //    @GetMapping("/hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "<h1>Hello from Spring!</h1>";
+//    }
+//    @PostMapping("/hello")
+//    @ResponseBody
+//    public String goodbye() {
+//        return "<h1>Goodbye from Spring!</h1>";
+//    }
+//    @GetMapping("/goodbye")
+//    @ResponseBody
+//    public String bye() {
+//        return "<h1>Bye from Spring!</h1>";
+//    }
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model vmodel) {
+        vmodel.addAttribute("name", name);
+        return "hello";
     }
-    @PostMapping("/hello")
-    @ResponseBody
-    public String goodbye() {
-        return "<h1>Goodbye from Spring!</h1>";
-    }
-    @GetMapping("/goodbye")
-    @ResponseBody
-    public String bye() {
-        return "<h1>Bye from Spring!</h1>";
-    }
+
 }
 
