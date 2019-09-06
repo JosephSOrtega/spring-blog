655784287daf76c807c17e659f8343fbd71a0b19
JustinSOrtega <justinsortega@gmail.com>
2019-09-06 10:25:47 -0500

    messing with views

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -1,15 +1,27 @@
 package com.codeup.springblog.controllers;
-
+//package com.codeup.springblog.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class HomeController {
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String sayHello() {
+//        return "This is the landing page!";
+//    }
+//
+//}
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    @ResponseBody
-    public String sayHello() {
-        return "This is the landing page!";
-    }
 
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
+    }
 }
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -8,21 +8,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
+
     @GetMapping("/posts")
     @ResponseBody
     public String postsAll() {
-        return "Posts Index Page";
+        return"<h1>Posts Index Page<h1>" +
+                "<h2>Here are all the posts:<h2>" +
+                "<ul>" +
+                "<li>1</li>" +
+                "<li>1</li>" +
+                "<li>1</li>" +
+                "</ul>";
     }
+
     @GetMapping("/posts/{id}")
     @ResponseBody
     public String postsAll(@PathVariable Long id) {
-        return "Posts Page "+id;
+        return "Here is Posts Page "+id;
     }
+
     @GetMapping("/posts/create")
     @ResponseBody
     public String createPostForm() {
         return "View the form for creating a post";
     }
+
     @PostMapping("/posts/create")
     @ResponseBody
     public String createPost() {
