1867a8b422a8b0ea5acae91380c0375652c186b3
JustinSOrtega <justinsortega@gmail.com>
2019-09-09 11:44:19 -0500

    starting ex

--- src/main/java/com/codeup/springblog/Repos/PostRepository.java
+++ src/main/java/com/codeup/springblog/Repos/PostRepository.java
@@ -4,6 +4,15 @@ import com.codeup.springblog.Models.Post;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
-//save();
+    //save();
 //    delete();
+//    findAll();
+//    findOne();
+//    Custom:
+    Post findByTitle(String title);
+//   the above = select * from ads where title is ?
+
+
 }
+
+
