34b1c025b40d573e743b652c505604e118888d24
JustinSOrtega <justinsortega@gmail.com>
2019-09-12 11:23:06 -0500

    fixed maybe checking on master

--- src/main/java/com/codeup/springblog/Repos/ImageRepository.java
+++ src/main/java/com/codeup/springblog/Repos/ImageRepository.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ImageRepository extends JpaRepository<AdImage, Long> {
-    @Modifying
-    @Query(value = "insert into spring_adlister_db.ad_images (path, post_id) VALUES (?1, ?2);", nativeQuery = true)
-    void insertNewImages(@Param("path") String path, @Param("post_id") Long post_id);
+//    @Modifying
+//    @Query(value = "insert into a (path, post_id) VALUES (?1, ?2);", nativeQuery = true)
+//    void insertNewImages(@Param("path") String path, @Param("post_id") Long post_id);
 
 //    void save(String path, Long id);
 }
