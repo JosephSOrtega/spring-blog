0e7cfb1ae7538720094642f320c452ec57759a48
JustinSOrtega <justinsortega@gmail.com>
2019-09-18 11:40:35 -0500

    dep test

--- src/main/java/com/codeup/springblog/Models/Post.java
+++ src/main/java/com/codeup/springblog/Models/Post.java
@@ -1,14 +1,6 @@
 package com.codeup.springblog.Models;
 
-import com.sun.xml.internal.xsom.impl.scd.Step;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
 import javax.persistence.*;
-import javax.xml.soap.Text;
 import java.util.List;
 
 @Entity
